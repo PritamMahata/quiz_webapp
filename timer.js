8d65171d3e0654f368a9608d242e24c0ea3de662
Pritam <pritammahata12@gmail.com>
2024-03-29 03:13:34 +0530

    change timmer function

--- timer.js
+++ timer.js
@@ -1,3 +1,54 @@
+const bottom = document.querySelector(".bottom");
+var timerElement = document.getElementById("timestamp");
+
+window.onload = function () {
+  setTimer();
+  checkTime();
+  // setInterval(setTimer, 5000);
+};
+if (
+  localStorage.getItem("t_time") == null ||
+  localStorage.getItem("t_time") == "" ||
+  localStorage.getItem("t_time") == undefined
+) {
+  localStorage.setItem("t_time", 120);
+} else {
+  setTimer();
+}
+
+function setTimer() {
+  var sec = localStorage.getItem("t_time");
+  console.log(sec);
+  var timer = setInterval(function () {
+    sec--;
+    localStorage.setItem("t_time", sec);
+    console.log(sec);
+    if (sec <= 0) {
+      clearInterval(timer);
+      bottom.innerHTML = `
+            <div class="result" style="background-color: #f2f2f2; padding: 20px; border-radius: 5px; display:flex; flex-direction: column; justify-content:center; align-items:center; width:100%;">
+                <h2> Time Up</h2>
+                <h5>  🏆 Your Score:  Correct Answers</h5>
+                <p> 🎉 Congratulations for completing the quiz! 🎉 </p>
+            </div>
+        `;
+    } else {
+      // document.getElementById('countdown').innerHTML = sec + " seconds remaining";
+      var minutes = Math.floor(sec / 60);
+      var seconds = sec % 60;
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      timerElement.textContent = minutes + ":" + seconds;
+    }
+  }, 1000);
+}
+
+// localStorage.setItem('t_time',120);
+// var sec = localStorage.getItem('t_time');
+// console.log(sec);
+
 function checkTime() {
   console.log("Checking time...");
   var currentTime = new Date();
@@ -15,6 +66,6 @@ function checkTime() {
     document.getElementById("timestamp").innerText = timeString;
   } else {
     console.log("Time to hide the text");
-    document.getElementById("timestamp").innerText = "Text to hide";
+    document.getElementById("timestamp").innerText = "00 : 00";
   }
 }
