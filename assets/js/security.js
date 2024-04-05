fb5baafa27fd9fd4d0dd3c984884257016c5d5db
tuhin56789 <tuhinghosh0169@gmail.com>
2024-04-05 09:16:44 +0530

    Disable screen capture and prevent right-click context menu

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,40 +1,16 @@
 document.addEventListener("visibilitychange", function () {
     if (document.visibilityState === 'hidden') {
-        // User switched tabs, do something
         console.log('User switched tabs');
     } else {
         console.log('User came back to the tab');
     }
 });
 
-// document.addEventListener('keyup', (e)=>{
-//     navigator.clipboard.writeText('')
-//     alert('Screen capture is disabled for security reasons.')
-// })
+document.addEventListener('keyup', (e)=>{
+    navigator.clipboard.writeText('')
+    alert('Screen capture is disabled for security reasons.')
+})
 
-document.addEventListener("keyup", function (e) {
-    var keyCode = e.keyCode ? e.keyCode : e.which;
-            if (keyCode == 44) {
-                stopPrntScr();
-            }
-        });
-function stopPrntScr() {
-
-            var inpFld = document.createElement("input");
-            inpFld.setAttribute("value", ".");
-            inpFld.setAttribute("width", "0");
-            inpFld.style.height = "0px";
-            inpFld.style.width = "0px";
-            inpFld.style.border = "0px";
-            document.body.appendChild(inpFld);
-            inpFld.select();
-            document.execCommand("copy");
-            inpFld.remove(inpFld);
-        }
-       function AccessClipboardData() {
-            try {
-                window.clipboardData.setData('text', "Access   Restricted");
-            } catch (err) {
-            }
-        }
-        setInterval("AccessClipboardData()", 300);
+window.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+  });
